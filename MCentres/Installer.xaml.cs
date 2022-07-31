59b5bc75816939bb352e9718c40f4e46f34e91e2
donut <donut@DESKTOP-88OT8VV>
2022-07-31 12:19:10 +0530

    Add project files.

---
+++ MCentres/Installer.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Composition.SystemBackdrops;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using WinRT.Interop;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace MCentres
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class Installer : Page
+	{
+		public Installer()
+		{
+			this.InitializeComponent();
+			FileVersionInfo WAMSVer = FileVersionInfo.GetVersionInfo(@"C:\Windows\System32\Windows.ApplicationModel.Store.dll");
+			WindowsVersion.Text = WAMSVer.ToString();
+		}
+
+		private void BackButton_Click(object sender, RoutedEventArgs e)
+		{
+			rootFrame.Navigate(typeof(MainWindow));
+		}
+	}
+}
